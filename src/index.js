e2b2245a3d32bab1e5dc8508f3fd96738ebc8ad8
UYulong <1274583083@qq.com>
2022-08-04 11:46:56 +0800

    文件夹整理

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App"; // 基础语法
-// import App from "./01-state"; // 状态和生命周期
-// import App from "./02-event"; // 事件处理
-// import App from "./03-condition"; // 条件渲染
-// import App from "./04-form"; // 表单
-// import App from "./05-state-up"; // 状态提升
-// import App from "./06-context"; // context 透传
-import App from "./07-props"; // props
+// import App from "./base/App"; // 基础语法
+// import App from "./base/01-state"; // 状态和生命周期
+// import App from "./base/02-event"; // 事件处理
+// import App from "./base/03-condition"; // 条件渲染
+// import App from "./base/04-form"; // 表单
+// import App from "./base/05-state-up"; // 状态提升
+// import App from "./base/06-context"; // context 透传
+import App from "./base/07-props"; // props
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
