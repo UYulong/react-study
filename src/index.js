d12fea667dc3863ecc5fab339be1689df38b1a97
UYulong <1274583083@qq.com>
2022-08-04 09:08:19 +0800

    context学习

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import ReactDOM from "react-dom/client";
 // import App from "./02-event"; // 事件处理
 // import App from "./03-condition"; // 条件渲染
 // import App from "./04-form"; // 表单
-import App from "./05-state-up"; // 状态提升
+// import App from "./05-state-up"; // 状态提升
+import App from "./06-context"; // context 透传
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
